ad164cd0b753fe0e3b735b162af6f556fbe85c23
Zach Whalen <zach.whalen@gmail.com>
2023-05-23 17:13:03 -0400

    emoji example

--- week2/2.1.emoji/sketch.js
+++ week2/2.1.emoji/sketch.js
@@ -4,4 +4,24 @@ function setup() {
 
 function draw() {
   background(220);
+
+
+  // outline for the face
+  stroke("black");
+  strokeWeight(6);
+
+  // the base face circle
+  fill("yellow");
+  ellipse(200,200,250,250);
+
+  // the eyes
+  fill("black");
+  stroke("black");
+  strokeWeight(1);
+  ellipse(160,150,25,75); // left eye
+  ellipse(240,150,25,75); // right eye
+
+  // the mouth
+  ellipse(200,260,120,40);
+
 }
