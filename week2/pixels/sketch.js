672c0ba2b6e416bf3e81155d74e44ee198d168e8
Zach Whalen <zach.whalen@gmail.com>
2023-05-26 12:28:39 -0400

    optimus prime example

--- week2/pixels/sketch.js
+++ week2/pixels/sketch.js
@@ -1,18 +1,20 @@
 let img;
-let sampleX = 40;
-let sampleY = 40;
+let sampleX = 20;
+let sampleY = 20;
 
 function preload(){
-  img = loadImage('https://i.kym-cdn.com/entries/icons/original/000/000/166/keyboar.jpg');
+  img = loadImage('https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png');
 }
-
 function setup() {
   createCanvas(800, 800);
   // noLoop();
+  img.loadPixels();
+  rectMode(CENTER);
 }
 
 function draw() {
-  background(220);
+  background("#444444");
+  noStroke();
 
   for (let x = 0; x <= img.width; x += sampleX){
     for (let y = 0; y <= img.height; y += sampleY){
@@ -21,12 +23,11 @@ function draw() {
       var g = img.pixels[index + 1];
       var b = img.pixels[index + 2];
 
+      var bright = ((0.3 * r) + (0.59 * g) + (0.11 * b))
+
       fill(r,g,b);
       rect(x,y,sampleX,sampleY);
 
     }
   }
-
-
-
 }
